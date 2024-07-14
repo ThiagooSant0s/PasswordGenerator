3d31c30e35e49c7e2189a407d1ec577868e20da9
Thiago Santos <94208348+ThiagooSant0s@users.noreply.github.com>
2024-07-13 21:28:32 -0300

    Update

--- script.js
+++ script.js
@@ -6,52 +6,56 @@ let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
-let charset ="abcdefghijklmnopqrstuvxwyzABCDEFGHIJKLMNOPQRSTUVXYWZ0123456789!@#$%&*_";
+let lowercase = "abcdefghijklmnopqrstuvwxyz";
+let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let numbers = "0123456789";
+let special = "!@#$%&*_";
+let charset = lowercase + uppercase + numbers + special;
 let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
 
-
-sliderElement.oninput = function(){
+sliderElement.oninput = function() {
     sizePassword.innerHTML = this.value;
 }
 
-function generatePassword(){
-
+function generatePassword() {
     let pass = "";
+    let length = parseInt(sliderElement.value);
 
-    for ( let i = 0, n = charset.length; i < sliderElement.value; ++i){
-        pass += charset.charAt(Math.floor(Math.random() * n));
+    // Garantir pelo menos um de cada tipo de caractere
+    pass += lowercase.charAt(Math.floor(Math.random() * lowercase.length));
+    pass += uppercase.charAt(Math.floor(Math.random() * uppercase.length));
+    pass += numbers.charAt(Math.floor(Math.random() * numbers.length));
+    pass += special.charAt(Math.floor(Math.random() * special.length));
+
+    // Completar o restante da senha aleatoriamente
+    for (let i = pass.length; i < length; ++i) {
+        pass += charset.charAt(Math.floor(Math.random() * charset.length));
     }
+
+    // Embaralhar os caracteres da senha para evitar que os obrigatórios fiquem sempre nas primeiras posições
+    pass = pass.split('').sort(() => 0.5 - Math.random()).join('');
 
     containerPassword.classList.remove("hide");
     password.innerHTML = pass;
     novaSenha = pass;
-    
-    }  
+}
 
-    function copyPassword(){
-        var passwordElement = document.getElementById('password');
-        var passwordText = passwordElement.textContent || passwordElement.innerText;
+function copyPassword() {
+    var passwordElement = document.getElementById('password');
+    var passwordText = passwordElement.textContent || passwordElement.innerText;
 
-        if (!navigator.clipboard){
-            alert('Seu navegador não suporta a funcionalidade de copiar para área de transferência.Você pode selecionar a senha manualmente e copiá-la');
-            return;
+    if (!navigator.clipboard) {
+        alert('Seu navegador não suporta a funcionalidade de copiar para área de transferência. Você pode selecionar a senha manualmente e copiá-la');
+        return;
+    }
 
-        }  
+    navigator.clipboard.writeText(passwordText).then(function() {
+        alert('Senha copiada para a área de transferência!');
+    }, function(err) {
+        console.error('Erro ao copiar a senha:', err);
+    });
+}
 
-        navigator.clipboard.writeText(passwordText).then(function(){
-            alert('Senha copiada para a área de tranferência!');
-
-        }, function(err){
-            console.error('Erro ao copiar a senha:' , err);
-        }); 
-        
-
-    }  
-
-
-
-
-    
-    
+buttonElement.addEventListener("click", generatePassword);
